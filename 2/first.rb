b8a4ab00af679ee5d2ece5cc55f47d9cb2678346
Matthew Black <matthewallenblack@gmail.com>
2017-12-21 15:38:54 -0600

    Day Two, Part One: Complete.

---
+++ 2/first.rb
@@ -0,0 +1,15 @@
+sheet = File.open("spreadsheet.txt").map do |line|
+  line.split.map { |num| num.to_i }
+end
+
+def solve_that_checksum(spreadsheet)
+  each_rows_largest_difference(spreadsheet).reduce(:+)
+end
+
+def each_rows_largest_difference(spreadsheet)
+  spreadsheet.map do |row|
+    row.max - row.min
+  end
+end
+
+p solve_that_checksum(sheet)
